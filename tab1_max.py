83814f3c1b780c34b1ad859d29a87996a3f375f4
ssesselmann <75355962+ssesselmann@users.noreply.github.com>
2025-07-04 19:23:24 +1000

    Update tab1_max.py

--- tab1_max.py
+++ tab1_max.py
@@ -1,10 +1,15 @@
 #tab3_max.py
 
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QFileDialog
 import pyqtgraph as pg
 import numpy as np
 import shared  # your shared data
 import threading
+
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QFileDialog
+from PySide6.QtGui import QFont, QBrush, QColor, QIntValidator, QPixmap
+
+from shared import logger, P1, P2, H1, H2, MONO
+
 
 class Tab1MaxWidget(QWidget):
     def __init__(self):
