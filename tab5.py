112ecdf1a0156cbd398812405c44475551fecdc8
ssesselmann <75355962+ssesselmann@users.noreply.github.com>
2025-07-04 19:23:49 +1000

    Update tab5.py

--- tab5.py
+++ tab5.py
@@ -1,11 +1,14 @@
 # tab5
 
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PySide6.QtGui import QFont, QBrush, QColor, QIntValidator, QPixmap
+from shared import logger, P1, P2, H1, H2, MONO
+
 
 class Tab5(QWidget):
     def __init__(self):
         super().__init__()
 
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("This is the Manual."))
-        self.setLayout(layout)
+        tab5_layout = QVBoxLayout()
+        tab5_layout.addWidget(QLabel("This is the Manual."))
+        self.setLayout(tab5_layout)
