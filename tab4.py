35a671258f9597beb8a3c029e73839b756b3d319
ssesselmann <75355962+ssesselmann@users.noreply.github.com>
2025-07-04 19:23:46 +1000

    Update tab4.py

--- tab4.py
+++ tab4.py
@@ -1,11 +1,13 @@
 #tab4.py
 
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QFileDialog
+from PySide6.QtGui import QFont, QBrush, QColor, QIntValidator, QPixmap
+from shared import logger, P1, P2, H1, H2, MONO
 
 
 
 class Tab4(QWidget):
     def __init__(self):
         super().__init__()
-        layout = QVBoxLayout(self)
+        tab4_layout = QVBoxLayout(self)
 
